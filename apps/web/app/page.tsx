178d4620d8a0f18cd3ea64b8e6730c8746cc3eb2
chaturvedi-anil <chaturvedia560@gmail.com>
2025-04-16 00:34:54 +0530

    feat: updated frontend

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,9 +2,9 @@ import { prismaClient } from "@repo/db/prismaClient";
 export default async function Home() {
   const user = await prismaClient.user.findFirst();
   return (
-    <div>
-      <p>email : {user?.email}</p>
-      <p>password : {user?.password}</p>
+    <div style={{border: "2px solid red", padding: "10px"}}>
+      <p style={{color: "red"}}>Email : {user?.email}</p>
+      <p style={{color: "royalblue"}}>Password : {user?.password}</p>
     </div>
   );
 }
