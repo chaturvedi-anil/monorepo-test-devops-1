f820fb136fe98c70e4e69095dc1b763aaa0c8ed4
chaturvedi-anil <chaturvedia560@gmail.com>
2025-04-16 13:37:53 +0530

    add some style

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,8 +2,8 @@ import { prismaClient } from "@repo/db/prismaClient";
 export default async function Home() {
   const user = await prismaClient.user.findFirst();
   return (
-    <div>
-      <h1> Testing </h1>
+    <div style={{display: "flex", justifyItems: "center", alignItems: "center"}}>
+      <h1 style={{color: "red"}}> Testing </h1>
       <p>user email : {user?.email}</p>
       <p>password : {user?.password}</p>
     </div>
